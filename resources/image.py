164ddf2916d09870b18ebd98010c0985e8784b9c
soej24 <soej24@naver.com>
2022-06-24 11:10:28 +0900

    add

--- resources/image.py
+++ resources/image.py
@@ -1,11 +1,14 @@
 from datetime import datetime
 from http import HTTPStatus
 from os import access
-from flask import Config, request
+from flask import request
 from flask_jwt_extended import create_access_token, get_jwt, get_jwt_identity, jwt_required
 from flask_restful import Resource
 from mysql.connector.errors import Error
-import mysql.connector
+
+import boto3
+
+from config import Config
 
 class FileUploadResource(Resource) :
 
@@ -13,31 +16,42 @@ class FileUploadResource(Resource) :
 
         # 1. 클라이언트로부터 데이터를 받아온다.
         # request.files 에 파일을 받아온다.
-        # 따라서 파일이 없는 상태로 API 가 호출되면,
-        # 에러메세지를 클라이런트에 응답해주자.
+        # 따라서 파일이 없는 상태로 API 가 호출되면, 에러메세지를
+        # 클라이언트에 응답해주자.
 
-        # photo란 클라이언틍서 보내는 key!!
+        # photo란, 클라이언트에서 보내는 key !!
         if 'photo' not in request.files:
             return {'error' : '파일을 업로드 하세요'}, 400
-
+        
         # 클라이언트로부터 파일을 받아온다.
         file = request.files['photo']
 
-        # 파일명을 우리가 변경해 준다. (파일명이 중복되서 올라가면 안되기 때문에~)
-        # 파일명은 유니크하게 만들어야 한다.
+        # 파일명을 우리가 변경해 준다.
+        # 파일명은, 유니크하게 만들어야 한다.
         current_time = datetime.now()
-        new_file_name = current_time.isoformat().replace(':', '_')
+        new_file_name = current_time.isoformat().replace(':', '_') + '.jpg'
 
-        # 유저가 올린 파일의 이름을 내가 만든 파일 명으로 변경
+        # 유저가 올린 파일의 이름을, 내가 만든 파일명으로 변경
         file.filename = new_file_name
 
-        # S3에 업로드 하면 된다.
-        # AWS의 라이브러리를 사용해야 한다.
-        # 이 파이썬 라이브러리를 boto3 라이브러리!
+        # S3 에 업로드 하면 된다.
+        # AWS 의 라이브러리를 사용해야 한다.
+        # 이 파이썬 라이브러리가 boto3 라이브러리다!
         # boto3 라이브러리 설치
-        # pip install boto3
+        # pip install boto3 
 
-        boto3.client('s3',
-                    aws_access_key_id = Config.ACCess_KEY)
+        s3 = boto3.client('s3', 
+                    aws_access_key_id = Config.ACCESS_KEY,
+                    aws_secret_access_key = Config.SECRET_ACCESS)
 
-        return
+        try :
+            s3.upload_fileobj(file,
+                                Config.S3_BUCKET,
+                                file.filename,
+                                ExtraArgs = {'ACL':'public-read', 'ContentType':file.content_type} )                 
+
+        except Exception as e :
+            return {'error' : str(e)}, 500
+
+        return {'result' : 'success', 
+                'imgUrl' : Config.S3_LOCATION + file.filename}
